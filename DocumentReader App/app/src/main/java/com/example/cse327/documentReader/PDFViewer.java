ea849f26494b76ba61e96705ae498a7fb84ecc55
Farhan Tanvir <tanvir.farhan@northsouth.edu>
2018-04-08 04:40:44 +0600

    Added initial eye tracking

--- DocumentReader App/app/src/main/java/com/example/cse327/documentReader/PDFViewer.java
+++ DocumentReader App/app/src/main/java/com/example/cse327/documentReader/PDFViewer.java
@@ -1,19 +1,35 @@
 package com.example.cse327.documentReader;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 
-public class PDFViewer extends AppCompatActivity {
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.IOException;
+
+public class PDFViewer extends AppCompatActivity{
 
     PDFView pdfView;
     private static  final String[] PERMISSION= new String[]{Manifest.permission.CAMERA};
     private static  final int INT_VALUE=11;
+    private FaceDetector faceDetector;
+    private FaceAndEyeTracker faceAndEyeTracker;
+    private CameraSource cameraSource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +37,32 @@ public class PDFViewer extends AppCompatActivity {
         pdfView=(PDFView)findViewById(R.id.pdfView);
         pdfView.fromAsset("sample.pdf").load();
         requestPermissions(PERMISSION,INT_VALUE);
+        //EventBus.getDefault().register(this);
+        Track();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void FaceNotFocusingEvent(FaceNotFoucingOnScreen event)
+    {
+        Log.d("Facetracking", "onUpdate: Face Is Pointing elsewhere");
+    }
+    @Subscribe
+    public void EyesClosedEvent(EyesClosedEvent event)
+    {
+        Log.d("eyes closed","Closed");
+        //Toast.makeText(this,"Your left eye is closed",Toast.LENGTH_LONG).show();
     }
 
     //Reference : https://developer.android.com/training/permissions/requesting.html?#java
@@ -30,8 +72,10 @@ public class PDFViewer extends AppCompatActivity {
             case INT_VALUE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //facetracking code will go here
-                } else {
+                    startCamera();
+                    return;
+                }
+                else {
                     AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
                     alertDialog.setTitle("Permission for camera")
                             .setMessage("Permission needed for eye tracking while reading")
@@ -41,10 +85,65 @@ public class PDFViewer extends AppCompatActivity {
                                     requestPermissions(PERMISSION, INT_VALUE);
                                 }
                             })
+                            .setCancelable(false)
                             .show();
+                    return;
                 }
             }
-            return;
         }
     }
+
+    public void startCamera(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED)
+        {
+            try{
+                cameraSource.start();
+                Toast.makeText(this,"Camera started",Toast.LENGTH_LONG).show();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this,"Camera not started",Toast.LENGTH_LONG).show();
+            }
+        }
+        return;
+    }
+
+    public void Track(){
+        /*
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED)
+        {
+            try{
+                cameraSource.start();
+            }
+            catch (IOException e) {
+                Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+            }
+
+            if(faceAndEyeTracker.eyesClosed)
+                Toast.makeText(this,"Your eyes are closed",Toast.LENGTH_LONG).show();
+        }
+         */
+        faceAndEyeTracker=new FaceAndEyeTracker();
+        faceDetector=new FaceDetector.Builder(getApplicationContext())
+                .setProminentFaceOnly(true)
+                .setTrackingEnabled(true)
+                .setMode(FaceDetector.FAST_MODE)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+               .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        faceDetector.setProcessor(
+                new LargestFaceFocusingProcessor.Builder(
+                        faceDetector,faceAndEyeTracker).build());
+
+        if(faceDetector.isOperational())
+            Toast.makeText(this,"Working",Toast.LENGTH_LONG).show();
+
+        cameraSource=new CameraSource.Builder(this,faceDetector)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(5f)
+                .setRequestedPreviewSize(640,480)
+                .build();
+    }
+
 }
